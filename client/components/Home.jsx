7b0d640159ceaf2c1c6571e870322d687ea51681
matthew-devonport <roarsintheeast@gmail.com>
2020-06-12 17:38:16 +1200

    draft bottom body done

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -16,12 +16,33 @@ class Home extends React.Component {
                         <h1>GREETINGS FROM SUN CITY PANEL &amp; PAINT</h1>
                         <div className="bodyFlex">
                             <p className="bodyText bodyItems">
-                                Here is some introduction information you can put in here<br /><br />
-This is also an image you can put in this part as well on the right.<br /><br />
-This will be longer in height (depending on how much information you really want here).
-Although as you can see the text wraps down from the image so it all depends on what you want.
-</p>
+                            Here is some introduction information you can put in here<br /><br />
+                            This is also an image you can put in this part as well on the right.<br /><br />
+                            This will be longer in height (depending on how much information you really want here).
+                            Although as you can see the text wraps down from the image so it all depends on what you want.
+                            </p>
                             <img src="./img/shop.jpg" alt="Shop Pic" className="bodyItems"></img>
+                        </div>
+                        <div className="bodyRow">
+                        <div className="bodyInfo">
+                            <img src="./img/link-placeholder.jpg" alt="Services Link" className="bottomPic"></img>
+                            <div className="bottomText">
+                                <h2>SERVCIES</h2>
+                                <p className="bottomInfo">
+                                We offer a range of services. Come and check them out!
+                                </p>
+                                </div>
+                            </div>
+                            <div className="bodyInfo">
+                            <img src="./img/link-placeholder.jpg" alt="Services Link" className="bottomPic"></img>
+                            <div className="bottomText">
+                            <h2>ABOUT US</h2>
+                            <p className="bottomInfo">
+                            Find out more about our business and the team!
+                            </p>
+                                </div>
+
+                        </div>
                         </div>
                     </div>
                     {/* <Footer /> */}
