8813ba0f448448f0ad8c444a99d53ccc193e911d
matthew-devonport <roarsintheeast@gmail.com>
2020-06-12 12:28:28 +1200

    setup basic home

---
+++ client/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Title from './Title'
+import Nav from './Nav'
+import Footer from './Footer'
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment> 
+                    <Nav/>
+                <div>
+                    <Title />
+                    <Footer />
+                </div>  
+            </React.Fragment>
+        )
+    }
+}
+
+export default Home
